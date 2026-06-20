af50975feed503bf47fefecf93c1fe19b218a926
Das <debajyoti.das@brightlysoftware.com>
2026-06-21 00:10:14 +0530

    chore: add custom domain dasdebajyoti.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://AndromedaIsHere.github.io',
-  base: '/Deb-Portfolio',
+  site: 'https://dasdebajyoti.com',
+  base: '/',
   output: 'static',
 });
